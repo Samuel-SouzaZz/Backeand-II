5c1caede6f51e44eed9bd776079615e1224d3ff1
Samuel-SouzaZz <samucael8832@gmail.com>
2025-09-14 11:14:57 -0300

    add inicial server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log("Hello World");
+import express from "express";
+import { config } from "dotenv";
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => { console.log(`listening on port ${PORT}!`); });
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+
+
+
+
+
+
